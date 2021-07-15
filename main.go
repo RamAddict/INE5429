3631a331f3a265ab464fe15b9cf7a8ea37b0e78e
RamAddict <apbiancofriber@gmail.com>
2021-07-15 12:56:51 -0300

    fix blum blum shub to use big int

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/big"
 	"time"
 )
 
@@ -9,28 +10,23 @@ import (
 // @param
 	p and q are primes that should both be congruent to 3 (mod 4)
 */
-func blumblumshub(p uint64, q uint64, seed uint64, nBits int) []uint32 {
-	var nOf32Values int = nBits / 32
-	var mod32 int = int(nBits % 32)
-	if mod32 != 0 {
-		nOf32Values += 1
-	}
+func blumblumshub(pa int64, qa int64, seed int64, nBits uint) *big.Int {
 
-	m := p * q
+	p := big.NewInt(pa)
+	q := big.NewInt(qa)
+	m := p.Mul(p, q)
 
-	var nBitArray = make([]uint32, nOf32Values)
-	nBitArray[0] |= uint32(seed >> 31)
-	xn := seed
-	for i := 0; i != nBits; i++ {
-		xnPlus1 := uint64(uint64(xn*xn) % m)
-		xn = xnPlus1
-		if xnPlus1%2 == 0 {
-			nBitArray[i/32] |= (uint32(0) << (i % 32))
-		} else {
-			nBitArray[i/32] |= (uint32(1) << (i % 32))
-		}
+	ret := big.NewInt(0)
+	xn := big.NewInt(seed)
+	for i := uint(0); i != nBits; i++ {
+		xn.Set(xn.Mul(xn, xn).Mod(xn, m))
+		mod2 := big.NewInt(1)
+		mod2.Mod(xn, big.NewInt(2))
+
+		ret = mod2.Or(ret, (mod2.Lsh(mod2, i)))
+
 	}
-	return nBitArray
+	return ret
 }
 
 func xorshift32by32(nBits int) []uint32 {
@@ -61,10 +57,38 @@ func xorshift32by32(nBits int) []uint32 {
 	return nBitArray
 }
 
+func millerRabin() {
+
+	// trash := big.NewInt(1)
+
+	// a := big.NewInt(1822323232323232328)
+	// fmt.Printf("blumblumshub(11, 23, 3, 56): %32b\n", a)
+	// b := big.NewInt(10)
+	// c := big.NewInt(5)
+	// fmt.Printf("blumblumshub(11, 23, 3, 56): %32b\n", b)
+	// fmt.Printf("blumblumshub(11, 23, 3, 56): %32b\n", c)
+	// fmt.Printf("blumblumshub(11, 23, 3, 56): %32b\n", trash)
+
+	// trash.Mul(b,c)
+	// fmt.Printf("blumblumshub(11, 23, 3, 56): %32b\n", a)
+	// fmt.Printf("blumblumshub(11, 23, 3, 56): %32b\n", b)
+	// fmt.Printf("blumblumshub(11, 23, 3, 56): %32b\n", c)
+	// fmt.Printf("blumblumshub(11, 23, 3, 56): %32b\n", trash)
+	// trash.Mul(trash,c)
+	// fmt.Printf("blumblumshub(11, 23, 3, 56): %32b\n", a)
+	// fmt.Printf("blumblumshub(11, 23, 3, 56): %32b\n", b)
+	// fmt.Printf("blumblumshub(11, 23, 3, 56): %32b\n", c)
+	// fmt.Printf("blumblumshub(11, 23, 3, 56): %32b\n", trash)
+
+	return
+}
+
 func main() {
 	// var bitSizeArray = []int {40, 56, 80, 128, 168, 224, 256, 512, 1024, 2048, 4096}
 	fmt.Println("begin")
-	fmt.Println(xorshift32by32(8))
-	fmt.Printf("blumblumshub(11, 23, 3, 56): %32b\n", blumblumshub(5807, 6287, 32, 4096))
-	fmt.Println(blumblumshub(5807, 6287, 32, 56))
+	fmt.Printf("blumblumshub(30000000091, 40000000003, 4882516701, 100): %32b\n", blumblumshub(30000000091, 40000000003, 4882516701, 4096))
+	// millerRabin()
+	// fmt.Println(xorshift32by32(8))
+	// fmt.Println(blumblumshub(5807, 6287, 32, 56))
+	return
 }
