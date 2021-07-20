857b838947084a3a5d5ff961011d1ed04510b149
RamAddict <apbiancofriber@gmail.com>
2021-07-20 17:24:19 -0300

    add documentation, set first bit in rand generators to 1

--- main.go
+++ main.go
@@ -30,19 +30,28 @@ func blumblumshub(pa int64, qa int64,
 
 		ret = mod2.Or(ret, (mod2.Lsh(mod2, i)))
 	}
+	// set left most bit to 1
+	mask := big.NewInt(1).Lsh(big.NewInt(1), uint(nBits))
+	ret.Or(ret, mask)
 	return ret
 }
-
+/*
+// Param
+	nBits is the size in bits of the output number
+// Return
+	the random number
+*/
 func xorshift32by32(nBits int) *big.Int {
 	// the idea here is to calculate the xorshift of the highest 32 bit multiple
-	// then shift right to get the required bit ammount
+	// then shift right to get the required bit amount
 	var nOf32Values int = nBits / 32
 	var mod32 int = int(nBits % 32)
 	if mod32 != 0 {
 		nOf32Values += 1
 	}
-
+ 
 	ret := big.NewInt(0)
+	// seed is hard coded as the time
 	seed := uint(time.Now().UnixNano())
 	seed ^= seed << 13
 	seed ^= seed >> 17
@@ -55,16 +64,18 @@ func xorshift32by32(nBits int) *big.Int {
 		seed ^= seed << 13
 		seed ^= seed >> 17
 		seed ^= seed << 5
-
+ 
 		ret.Lsh(ret, uint(32*i))
 		ret.Or(ret, big.NewInt(int64(seed>>32))) // force 32 bits
 	}
-
-	// shifting right to get the required bit ammount
+ 
+	// shifting right to get the required bit amount
 	if mod32 != 0 {
 		ret.Rsh(ret, uint(32-mod32))
 	}
-
+	// set left most bit to 1
+	mask := big.NewInt(1).Lsh(big.NewInt(1), uint(nBits))
+	ret.Or(ret, mask)
 	return ret.Add(ret, big.NewInt(1))
 }
 
@@ -89,15 +100,26 @@ func eq(a *big.Int, b *big.Int) bool {
 	// 0 if x == y
 	// +1 if x > y
 }
-
+/*
+// Param
+	nPossiblePrime is the number that will be tested
+	attemps is the number of certainty iterations
+// Return
+	if the number is prime
+*/
 func  millerRabin(nPossiblePrime *big.Int, attempts uint) bool {
 
+	// if less than 1 
+	if lt(nPossiblePrime, big.NewInt(1)) {
+        return false
+    }
+	
 	// if even, stop
     if eq(big.NewInt(0).Mod(nPossiblePrime, big.NewInt(2)), big.NewInt(0)) {
         return false
     }
 	// obviously prime
-	if eq(nPossiblePrime, big.NewInt(2)) || eq(nPossiblePrime, big.NewInt(3)) || eq(nPossiblePrime, big.NewInt(5)){
+	if eq(nPossiblePrime, big.NewInt(1)) || eq(nPossiblePrime, big.NewInt(2)) || eq(nPossiblePrime, big.NewInt(3)) || eq(nPossiblePrime, big.NewInt(5)){
         return true
     }
 	// n -1
@@ -141,6 +163,13 @@ func  millerRabin(nPossiblePrime *big.Int, attempts uint) bool {
     return true
 }
 
+/*
+// Param
+	nPossiblePrime is the number that will be tested
+	attemps is the number of certainty iterations
+// Return
+	if the number is prime
+*/
 func fermatTest(possiblePrime *big.Int, attempts uint) bool {
 
     pMinus1 := big.NewInt(0).Sub(possiblePrime, big.NewInt(1))
@@ -212,6 +241,36 @@ func main() {
 	        avgTime+=time
 	    }
 	    fmt.Printf("blum blum shub ,%v ,%v \n",size, (avgTime/500))
+	}
+	
+	for _, size := range bitSizeArray {
+	    var avgTime time.Duration = 0
+	    for i := 0; i != 1; i++ {
+	        potentialPrime := xorshift32by32(size)
+			start := time.Now()
+	        if millerRabin(potentialPrime, 20) {
+				fmt.Printf("millerRabin says %v  is prime!\n", potentialPrime)
+			}
+	        end := time.Now()
+	        time := end.Sub(start)
+	        avgTime+=time
+	    }
+	    fmt.Printf("MillerRabin ,%v ,%v \n",size, (avgTime/1))
+	}
+
+	for _, size := range bitSizeArray {
+	    var avgTime time.Duration = 0
+	    for i := 0; i != 5; i++ {
+	        potentialPrime := xorshift32by32(size)
+			start := time.Now()
+	        if fermatTest(potentialPrime, 20) {
+				fmt.Printf("fermatTest says %v  is prime!\n", potentialPrime)
+			}
+	        end := time.Now()
+	        time := end.Sub(start)
+	        avgTime+=time
+	    }
+	    fmt.Printf("Fermat ,%v ,%v \n",size, (avgTime/1))
 	}
 
 	// fmt.Printf("blumblumshub(30000000091, 40000000003, 4882516701, 100): %32b\n", blumblumshub(30000000091, 40000000003, 4882516701, 4096))
